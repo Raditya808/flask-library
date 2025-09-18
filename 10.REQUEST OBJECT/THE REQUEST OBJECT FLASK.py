aa2104095aea9dd75f9ffdf0148133903ad1da5b
Raditya808 <mraditradit808@gmail.com>
2025-09-18 16:46:39 +0700

    error in line 46 and 53 eror tidak masuk di web

--- 10.REQUEST OBJECT/THE REQUEST OBJECT FLASK.py
+++ 10.REQUEST OBJECT/THE REQUEST OBJECT FLASK.py
@@ -1,15 +1,19 @@
 from flask import Flask , request, render_template 
 
 app = Flask(__name__)
-
-
-def valid_login(username, password):
-    return username == 'admin' and password == 'secret'
-
-
-def log_the_user_in(username):
-    return f'Logged in as {username}'
-
+#========================================================================
+#                                                                       =
+# contoh routing pertama menggunakan file templates dari hhtml          =
+#                                                                       =
+#========================================================================
+def valid_login(username, password):                    #================
+    return username == 'admin' and password == 'secret' #================
+                                                        #================
+                                                        #================
+def log_the_user_in(username):                          #================
+    return f'Logged in as {username}'                   #================
+                                                        #================
+#========================================================================
 @app.route('/',methods=['GET','POST'])
 def login():
     error = None
@@ -22,6 +26,41 @@ def login():
     # the code below is executed if the request method
     # was GET or the credentials were invalid
     return render_template('login.html', error=error)
+#========================================================================
+#                                                                       =
+# contoh tanpa menggunakan file templates                               =
+# isue dengan kode error tidak keluar di web                            =
+#========================================================================
+def valid_Login(namapengguna, sandi):                   #================                   
+    return namapengguna == 'admin' and sandi == '12345' #================
+                                                        #================
+                                                        #================
+def user_login(namapengguna):                           #================
+    return f'''                                         
+                                                                
+    <h1>Welcome to website using flask without templates html file
+    '''                                                 #================
+#========================================================================
+@app.route('/login', methods=['GET','POST'])
+def masuk():
+    error = None
+    if request.method == "POST":
+        if valid_Login(request.form['namapengguna'],
+                       request.form['sandi']):
+            return user_login(request.form['namapengguna'])
+
+        else:
+            error = 'invalid nama pengguna / sandi'
+        
+    return f'''
+    <form method="POST" action="/login">
+Nama: <input type="teks" placeholder="Nama pengguna" 
+name="namapengguna"><br>
+password: <input type="password" placeholder="sandi" 
+name="sandi"><br>
+<input type="submit" value="submit">
+        ''' 
+#========================================================================
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
