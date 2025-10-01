277c3d5d7d44b589cc8d16ddf05aa6c6b937886b
Raditya808 <mraditradit808@gmail.com>
2025-10-01 15:35:32 +0700

    file upload and secure syntax

---
+++ 11.FILE UPLOAD/2/app.py
@@ -0,0 +1,65 @@
+# File syntax https://flask.palletsprojects.com/en/stable/patterns/fileuploads/
+
+# file secure
+
+# A gentle introduction 
+
+import os 
+
+from flask import Flask, flash, request, redirect, url_for ,send_from_directory 
+
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+ALLOWED_EXSTENSIONS = {'txt','pdf','png','jpg','jpeg','gif'}
+
+app = Flask(__name__)
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXSTENSIONS
+
+
+@app.route('/',methods =['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        # CHECH IF THE POST REQUEST HAS THE FILE PART
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If empty file without a file name 
+        # empty file without a filename
+        if file.filename == '':
+            flash('no file selected')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return f'''
+       <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/download/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+# Contoh kode sederhana untuk menampilkan suatu directory file ke browser dengan aman
+if __name__=='__main__':
+    (app.run(debug=True))
+
+
+
